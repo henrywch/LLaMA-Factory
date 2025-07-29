a9c2b47855a3749cdbe590529054e0e34c24c4a8
henrywch <2462977089@qq.com>
2025-07-29 09:27:34 +0800

    Image path altered.

--- examples/llava_qwen_tuning/accelerate.sh
+++ examples/llava_qwen_tuning/accelerate.sh
@@ -2,6 +2,6 @@ accelerate launch \
     --num_machines=1 \
     --num_processes=2 \
     --mixed_precision=fp16 \
+    --gpu_ids '0,1' \
     --dynamo_backend="no" \
-    --main_process_port=30000 \
     $(which llamafactory-cli) train examples/llava_qwen_tuning/llava_qwen_pretrain.yaml
